cc7f505eaeb1e628d438a8709e210e11aeed630e
sathya <sathya15130@gmail.com>
2014-09-14 16:41:24 +0700

    age
    age

---
+++ src/ageProblem/age.java
@@ -0,0 +1,17 @@
+package ageProblem;
+
+public class age {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age;
+		String name;
+		System.out.println("hello world");
+		age = 33;
+        System.out.println("your age is " +age);
+        name = "pooja";
+        System.out.println("your name is " +name);
+        
+	}
+
+}
